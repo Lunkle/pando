0b06e471d7df5de68139ac3284c078fc2f7d00ea
Lunkle <bobbob.donny@gmail.com>
2019-04-07 19:13:32 -0400

    Created the shutdown loop

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -17,6 +17,7 @@ import guis.GUIRenderer;
 import guis.GUITexture;
 import models.RawModel;
 import models.TexturedModel;
+import network.Client;
 import objConverter.ModelData;
 import objConverter.OBJFileLoader;
 import renderEngine.DisplayManager;
@@ -27,11 +28,11 @@ import textures.ModelTexture;
 import textures.TerrainTexture;
 import textures.TerrainTexturePack;
 import toolbox.MousePicker;
-import network.Client;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
+		Runtime.getRuntime().addShutdownHook(new ShutdownHook());
 		// main
 		Client client = new Client();
 		try {
@@ -39,7 +40,7 @@ public class MainGameLoop {
 		} catch (java.io.IOException io) {
 			System.out.println(io);
 		}
-		
+
 		DisplayManager.createDisplay();
 
 		ModelData fernData = OBJFileLoader.loadOBJ("fern");
@@ -52,8 +53,7 @@ public class MainGameLoop {
 
 		ModelData oakTreeStage1Data = OBJFileLoader.loadOBJ("oakTreeStage1");
 		RawModel rawOakTreeStage1Model = Loader.loadToVAO(oakTreeStage1Data);
-		TexturedModel oakTreeStage1Model = new TexturedModel(rawOakTreeStage1Model,
-				new ModelTexture(Loader.loadTexture("oakTreeStage1")));
+		TexturedModel oakTreeStage1Model = new TexturedModel(rawOakTreeStage1Model, new ModelTexture(Loader.loadTexture("oakTreeStage1")));
 
 //		ModelData oakTreeStage2Data = OBJFileLoader.loadOBJ("oakTreeStage1");
 //		RawModel rawOakTreeStage2Model = loader.loadToVAO(oakTreeStage2Data);
@@ -65,7 +65,7 @@ public class MainGameLoop {
 		List<Entity> ferns = new ArrayList<Entity>();
 		List<Entity> oaks = new ArrayList<Entity>();
 		Entity centerSprout = new Entity(oakTreeStage1Model, new Vector3f(0, 0, 0), 0, 0, 0, 1);
-		
+
 		TerrainTexture backgroundTexture = new TerrainTexture(Loader.loadTexture("green"));
 		TerrainTexture rTexture = new TerrainTexture(Loader.loadTexture("dirt"));
 		TerrainTexture gTexture = new TerrainTexture(Loader.loadTexture("pinkFlowers"));
@@ -87,15 +87,13 @@ public class MainGameLoop {
 			float entityX = 10 + random.nextFloat() * 50;
 			float entityZ = 10 + random.nextFloat() * 50;
 			float entityY = terrainData.getHeightByWorldCoords(entityX, entityZ);
-			oaks.add(new Entity(oakTreeStage1Model, new Vector3f(entityX, entityY, entityZ), 0,
-					random.nextFloat() * 360, 0, 3));
+			oaks.add(new Entity(oakTreeStage1Model, new Vector3f(entityX, entityY, entityZ), 0, random.nextFloat() * 360, 0, 3));
 		}
 
 		Light light = new Light(new Vector3f(20000, 20000, 2000), new Vector3f(1, 1, 1));
 
 		List<GUITexture> guis = new ArrayList<GUITexture>();
-		GUITexture gui = new GUITexture(Loader.loadTexture("grassy"), new Vector2f(0.5f, 0.5f),
-				new Vector2f(0.25f, 0.25f));
+		GUITexture gui = new GUITexture(Loader.loadTexture("grassy"), new Vector2f(0.5f, 0.5f), new Vector2f(0.25f, 0.25f));
 		guis.add(gui);
 
 		Player player = new Player(oakTreeStage1Model, new Vector3f(10, 0, 15), 0.0f, 0.0f, 0.0f, 1f);
@@ -121,10 +119,7 @@ public class MainGameLoop {
 			}
 			Vector3f pPos = player.getPosition();
 			Vector2f coords = terrainData.getHexagon(pPos.x, pPos.z);
-			centerSprout.setPosition(new Vector3f(
-					(coords.y % 2) * Terrain.HEX_HALF_SQRT3 + coords.x * Terrain.HEX_SQRT3 + Terrain.HEX_HALF_SQRT3,
-					terrainData.getHeightByHexCoords((int) coords.x, (int) coords.y),
-					coords.y * 1.5f * Terrain.HEX_SIDE + Terrain.HEX_SIDE));
+			centerSprout.setPosition(new Vector3f((coords.y % 2) * Terrain.HEX_HALF_SQRT3 + coords.x * Terrain.HEX_SQRT3 + Terrain.HEX_HALF_SQRT3, terrainData.getHeightByHexCoords((int) coords.x, (int) coords.y), coords.y * 1.5f * Terrain.HEX_SIDE + Terrain.HEX_SIDE));
 
 			masterRenderer.processEntity(centerSprout);
 
